2050dc55b879549ee51409c1afadaa60cbf427dd
valentinedwv <none@none>
2010-08-18 00:03:22 +0000

    --HG-- extra : convert_revision : svn%3A41f7e840-45ea-4bf8-acc0-4407660a91dc%40719

---
+++ sandbox/oddataWaterWebService/WaterDataAccess/VariablesData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cuahsi.Wof.WaterDataAccess
+{
+    public class VariablesData
+    {
+        
+    }
+}
