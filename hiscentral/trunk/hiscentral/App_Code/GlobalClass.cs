df55125238d09812c9fbb765f83926a972be9bca
valentinedwv <none@none>
2010-09-28 11:50:08 -0700

    Added Overall Method usage tracking, and uptime in ServiceStatistics.cs Added Global.asax, and GlobalClass.cs

---
+++ hiscentral/trunk/hiscentral/App_Code/GlobalClass.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Web;
+using System.Web.SessionState;
+using System.Configuration;
+using System.Net;
+using System.Xml;
+using log4net;
+
+/// <summary>
+/// Summary description for GlobalClass
+/// </summary>
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "hiscentral.log4net", Watch = true)]
+namespace HisCentral
+{
+
+    public class GlobalClass : System.Web.HttpApplication
+    {
+        private static readonly ILog log = LogManager.GetLogger("PerformanceLogger");
+
+       
+      
+        void Application_Start(object sender, EventArgs e)
+        {
+
+           
+            
+
+            // Code that runs on application startup
+            String codeDate = File.GetCreationTimeUtc(Assembly.GetExecutingAssembly().Location).ToString();
+
+            String process = "GenericODWS_Start";
+            String path = Context.Request.ApplicationPath;
+           
+
+            String contact = ConfigurationManager.AppSettings["contactEmail"];
+
+           
+            log4net.GlobalContext.Properties["contact"] = contact;
+
+            log4net.GlobalContext.Properties["path"] = path;
+
+            log4net.GlobalContext.Properties["codeDate"] = codeDate;
+            log.Info(process);
+            var stats = new ServiceStatistics();
+            Application.Add("ServiceStatistics", stats);
+  
+        }
+
+
+
+
+         void Application_End(object sender, EventArgs e)
+        {
+     
+            //  Code that runs on application shutdown
+            String process = "GenericODWS_Stop";
+            
+            log.Info(process);
+            log.Info(((ServiceStatistics)Application["ServiceStatistics"]).ToString());
+        }
+
+         void Application_Error(object sender, EventArgs e)
+        {
+          
+            // Code that runs when an unhandled error occurs
+            log.Error("Application Error");
+            log.Info(((ServiceStatistics)Application["ServiceStatistics"]).ToString());
+        }
+
+        void Session_Start(object sender, EventArgs e)
+        {
+            
+            log.Debug("sessionStart");
+
+            log.Info(((ServiceStatistics)Application["ServiceStatistics"]).ToString());
+        }
+
+         void Session_End(object sender, EventArgs e)
+        {
+            // Code that runs when a session ends. 
+            // Note: The Session_End event is raised only when the sessionstate mode
+            // is set to InProc in the Web.config file. If session mode is set to StateServer 
+            // or SQLServer, the event is not raised.
+            log.Debug("sessionEnd");
+            log.Info(((ServiceStatistics)Application["ServiceStatistics"]).ToString());
+        }
+    }
+}
---
+++ hiscentral/trunk/hiscentral/App_Code/ServiceStatistics.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Web;
+using log4net;
+using String = System.String;
+
+/// <summary>
+/// Summary description for ServiceStatistics
+/// </summary>
+public  class ServiceStatistics
+{
+    private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    private static readonly ILog PerformanceLog = LogManager.GetLogger("PerformanceLog");
+    private static  string _machineName = "UNSET";
+    private static Stopwatch timer = new Stopwatch();
+
+    private Dictionary<string, Int64> _methodCalls = new Dictionary<string, long>();
+
+     static ServiceStatistics()
+    {
+       timer.Start();
+    }
+
+    private Int64 _hitCount =0;
+
+     public static String Name { get { return "HISCentral"; } }
+    public  String MachineName
+    {
+        get
+    {
+        
+        return _machineName;
+    }
+        set { _machineName = value; }}
+    public  TimeSpan Uptime
+    {
+        get { return timer.Elapsed; }
+    }
+    public  Dictionary<string, Int64> MethodCalls
+    {
+        get { return _methodCalls; }  } 
+
+     public  void AddCount(string methodName)
+     {
+         _hitCount++;
+        if (MethodCalls.ContainsKey(methodName))
+        {
+            MethodCalls[methodName]++;
+        } else
+        {
+            MethodCalls.Add(methodName,1); 
+        }
+    }
+    
+    public override String ToString()
+    {
+        const string format = " [{0}:{1}] ";
+       StringBuilder message = new StringBuilder();
+        message.Append("[ServerStats: ");
+        message.AppendFormat(format, "Uptime", Uptime);
+        foreach (var methodCall in MethodCalls)
+        {
+            message.AppendFormat(format, methodCall.Key, methodCall.Value);
+        }
+        message.Append(" ]");
+
+        return message.ToString();
+    }
+}
