d5a03af4b11c475f3e8ed9b6eec2df3876d18e27
valentinedwv <none@none>
2011-09-07 18:37:21 -0700

    Add rest service prototypes.

---
+++ genericwebservices/trunk/GenericWs2/REST/IRest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WaterOneFlow.odws.v1_1.wcf.REST
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRest" in both code and config file together.
+    [ServiceContract]
+    public interface IRest
+    {
+        [OperationContract()]
+        [WebGet(UriTemplate = "/")]
+        void ListNetworks();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/{network}/capabilities")]
+        void NetworkCapabilites(string network);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/{network}/sites")]
+        void NetworkSites(string network);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/{network}/series")]
+        void NetworkSeries(string network);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/{network}/results")]
+        void NetworkData(string network);
+    }
+
+}
---
+++ genericwebservices/trunk/GenericWs2/REST/Rest.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WaterOneFlow.odws.v1_1.wcf.REST
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Rest" in code, svc and config file together.
+    public class Rest : IRest
+    {
+       
+        public void ListNetworks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NetworkCapabilites(string network)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NetworkSites(string network)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NetworkSeries(string network)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NetworkData(string network)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
