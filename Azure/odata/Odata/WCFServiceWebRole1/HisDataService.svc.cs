d34bb7e438e7a519e86869758fbb15cbbb879e9b
valentinedwv <none@none>
2012-01-06 17:09:49 -0800

    Add Odata in Azure example for his central

---
+++ Azure/odata/Odata/WCFServiceWebRole1/HisDataService.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace WCFServiceWebRole1
+{
+    public class HisDataService : DataService< hisEntities >
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+             config.UseVerboseErrors = true; // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+            // Examples:
+             config.SetEntitySetAccessRule("SeriesCatalogs", EntitySetRights.AllRead);
+             config.SetEntitySetAccessRule("Sites", EntitySetRights.AllRead);
+             config.SetEntitySetAccessRule("Variables", EntitySetRights.AllRead);
+          
+            // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+        }
+    }
+}
