cf3802266dd9084d4c7c58b05e23cdea7e6371c7
valentinedwv <none@none>
2012-04-12 12:03:28 -0700

    siteInfoDataset_v1_1.cs got deleted. adding it back

---
+++ genericwebservices/trunk/ODM1_1_Datasets/siteInfoDataSet_v1_1.cs
@@ -0,0 +1,13 @@
+﻿namespace WaterOneFlow.odm.v1_1 {
+    
+    
+    public partial class siteInfoDataSet {
+    }
+}
+
+namespace WaterOneFlow.odm.v1_1.siteInfoDataSetTableAdapters {
+    
+    
+    public partial class sitesTableAdapter {
+    }
+}
