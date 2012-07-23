42802acaec8556e84256336ae296135bd0723e33
Admin@Admin-PC.sdsc.edu <Admin@Admin-PC.sdsc.edu>
2012-07-23 12:50:12 -0700

    Everything

---
+++ ControlledVocabulary/HisTermsService/IVocabularyRest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using cuahsi.HisVocabLib;
+
+namespace cuahsi.his.vocabservice
+{
+    [ServiceContract(Namespace = "http://his.cuahsi.org/services/ontology")]
+    interface IVocabularyRest
+    {
+        [WebGet(UriTemplate = "/Vocabularies" )]  
+        Vocabulary[] getVocabularies();
+    }
+}
